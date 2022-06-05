098b4994dff8625e9aec9ea33854da01cfb4ece5
machinacyfrowa <38609429+machinacyfrowa@users.noreply.github.com>
2022-06-05 10:09:42 +0200

    mobile test

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>Druga linia tekstu</Text>
       <StatusBar style="auto" />
     </View>
   );
